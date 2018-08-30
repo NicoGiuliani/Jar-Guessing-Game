8df44585d57c6acb46a0184edbff79ff3b9329f2
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-08-30 14:58:22 -0700

    Added a comment.

--- Game.java
+++ Game.java
@@ -5,7 +5,8 @@ public class Game {
   public static void main(String[] args) {
     boolean keepPlaying = true;
     Jar mJar;
-    
+
+    // until the player has chosen to quit playing, continue initiating new rounds
     while (keepPlaying) {
       mJar = Prompter.promptForSetup();
       mJar.fill();
